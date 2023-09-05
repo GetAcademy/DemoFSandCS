02dd3aa7c7a00a882c41d2c5bdea1799d6f35f85
GET Academy <terje@kolderup.net>
2023-09-05 15:51:07 +0200

    Update Demo6.fs

--- ConsoleFS/Demo6.fs
+++ ConsoleFS/Demo6.fs
@@ -3,7 +3,7 @@
 open System
     let makeNameCaseWord (word:string) = 
         let firstLetter = Char.ToUpper word[0] 
-        firstLetter.ToString() + word.Substring(1).ToLower()
+        firstLetter + word.Substring(1).ToLower()
 
     let makeNameCase (name:string) =
         name.Split ' ' |> Array.map(makeNameCaseWord) |> String.concat " "  
