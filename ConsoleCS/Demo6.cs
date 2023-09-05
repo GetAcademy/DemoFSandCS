415e0cc4807b22016a88d6cd938d93e10a793de4
GET Academy <terje@getacademy.no>
2023-09-05 20:29:58 +0200

    x

---
+++ ConsoleCS/Demo6.cs
@@ -0,0 +1,79 @@
+﻿namespace ConsoleCS
+{
+    internal class Demo6
+    {
+        public static void Run()
+        {
+            // =>"Terje Albert Kolderup"
+            //Console.WriteLine(NameCaseImperative("tErjE AlbErt kOldErUp"));
+            //Console.WriteLine(NameCaseLinq("tErjE AlbErt kOldErUp"));
+            //Console.WriteLine(NameCase("tErjE AlbErt kOldErUp"));
+
+            var fixNameCase = Pipe(
+                Split(' '),
+                Map(Pipe(ToLower, FirstToUpper)),
+                Join(' ')
+            );
+            Console.WriteLine(fixNameCase("tErjE AlbErt kOldErUp"));
+
+            //Console.WriteLine(Casing.makeNameCase("tErjE AlbErt kOldErUp"));
+        }
+
+        // v1 imperative 
+        static string NameCaseImperative(string name)
+        {
+            var words = name.Split(' ');
+            var newName = "";
+            foreach (var word in words)
+            {
+                if (newName.Length > 0) newName += ' ';
+                newName += char.ToUpper(word[0]) + word.Substring(1).ToLower();
+            }
+            return newName;
+        }
+
+        // v2 litt mer funksjonell
+        static string NameCaseLinq(string name)
+        {
+            var newParts = name
+                .Split(' ')
+                .Select(part =>
+                {
+                    var lower = part.ToLower();
+                    return char.ToUpper(lower[0]) + lower.Substring(1);
+                });
+            return string.Join(' ', newParts);
+        }
+
+        // v3 funksjonell
+        static string NameCase(string text) => Pipe(
+                Split(' '),
+                Map(Pipe(ToLower, FirstToUpper)),
+                Join(' ')
+            )(text);
+
+
+        // naive functional helpers
+        static Func<string, string> Pipe(
+            Func<string, string[]> f1,
+            Func<string[], IEnumerable<string>> f2,
+            Func<IEnumerable<string>, string> f3)
+        {
+            return arg => f3(f2(f1(arg)));
+        }
+
+        static Func<string, string> Pipe(
+            Func<string, string> f1,
+            Func<string, string> f2)
+        {
+            return arg => f2(f1(arg));
+        }
+
+        // point free and curried string functions
+        static Func<char, Func<string, string[]>> Split = separator => text => text.Split(separator);
+        static Func<string, string> FirstToUpper = s => char.ToUpper(s[0]) + s.Substring(1);
+        static Func<string, string> ToLower = s => s.ToLower();
+        static Func<Func<string, string>, Func<string[], IEnumerable<string>>> Map = f => array => array.Select(e => f(e));
+        static Func<char, Func<IEnumerable<string>, string>> Join = separator => array => string.Join(separator, array);
+    }
+}
--- ConsoleFS/Demo6.fs
+++ ConsoleFS/Demo6.fs
@@ -1,9 +1,17 @@
 ﻿module Demo6
 
 open System
-    let makeNameCaseWord (word:string) = 
-        let firstLetter = Char.ToUpper word[0] 
-        firstLetter + word.Substring(1).ToLower()
 
-    let makeNameCase (name:string) =
-        name.Split ' ' |> Array.map(makeNameCaseWord) |> String.concat " "  
+let makeNameCaseWord (word:string) = 
+    match word with
+    | _ ->
+        let head = word.[0] |> Char.ToUpper
+        let tail = word.[1..].ToLower()
+        string head + tail
+
+let makeNameCase (name:string) =
+    name.Split ' ' |> Array.map(makeNameCaseWord) |> String.concat " "  
+
+let run () =
+    let result = makeNameCase "tErjE AlbErt kOldErUp"
+    printfn "%s" result
